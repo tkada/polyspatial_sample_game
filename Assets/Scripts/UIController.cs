7e87d6141dbfde40633d20a772b26831a8122e02
Takeshi Kada <t.kada.1985@gmail.com>
2024-02-26 16:35:57 +0900

    Sample消した

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// UIの制御
+/// </summary>
+public class UIController : MonoBehaviour
+{
+    [SerializeField] GameObject startButton;
+    [SerializeField] GameObject scoreObject;
+    [SerializeField] GameObject gameOverObject;
+
+    public void VisibleTitle(bool visible)
+    {
+        this.startButton.SetActive(visible);
+    }
+
+    public void VisibleScore(bool visible)
+    {
+        this.scoreObject.SetActive(visible);
+    }
+
+    public void VisibleGameOver(bool visible)
+    {
+        this.gameOverObject.SetActive(visible);
+    }
+}
